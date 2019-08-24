1842829070d1038e04cd8904dc286dc63ea71661
K1ng04 <shahshainil94@gmail.com>
2019-08-24 16:46:17 -0500

    Added Force.cpp

---
+++ Force.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// F = ma, where F = Force, M = mass
+
+
+int main(){
+    int mass;
+    float acceleration;
+    cout << "Enter mass: ";
+    cin >> mass;
+    cout << "Enter Acceleration: ";
+    cin >> acceleration;
+    cout << mass * acceleration << " m/s^2";
+
+}
